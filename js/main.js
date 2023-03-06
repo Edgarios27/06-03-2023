106e2709814dde6b49afb2a5199987b6abc12abd
Edgar Rios <edgarios2707@gmail.com>
2023-03-06 19:44:44 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let resultado = document.querySelector("#texto");
+let num1 = 0;
+let num2 = 0;
+function Sumar(){
+    num1 = num1 + 1;
+    resultado.innerHTML = num1;
+}
+function Restar(){
+    num1 = num1 - 1;
+    resultado.innerHTML = num1;
+}
